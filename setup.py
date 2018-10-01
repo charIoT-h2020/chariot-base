6c1c8e2417442a07638c6639a111511dae0349fd
George Theofilis <g.theofilis@clmsuk.com>
2018-10-01 13:05:29 +0000

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/chariot-h2020/chariot_base',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
