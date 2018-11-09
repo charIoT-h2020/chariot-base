04135adb67a6810ffe3c87d954a6bcaf57fa98ae
George Theofilis <g.theofilis@clmsuk.com>
2018-11-09 18:08:12 +0200

    ✅💥🏗 Modify how I write to the local storage

--- chariot_base/datasource/local.py
+++ chariot_base/datasource/local.py
@@ -4,8 +4,10 @@ from influxdb import InfluxDBClient
 
 class LocalDataSource(object):
 
-    def __init__(self):
-        self.db = InfluxDBClient('172.18.1.4', 8086, 'root', 'root', 'fog_logs')
+    def __init__(self, hostname, port, username, password, db_name, duration='4w'):
+        self.db = InfluxDBClient(hostname, port, username, password, db_name)
+        self.db.create_database(db_name)
+        self.db.create_retention_policy('awesome_policy', duration, 3, default=True)
 
     def publish(self, point):
         json_body = [
@@ -19,4 +21,7 @@ class LocalDataSource(object):
                 'fields': point.message
             }
         ]
-        self.db.write_points(json_body, database=point.db, protocol='json')
+        return self.db.write_points(json_body, protocol='json', retention_policy='awesome_policy')
+
+    def query(self, q, db_name):
+        return self.db.query(q, database=db_name)
