e4b6a04369b12e2ccf79bc2c377322a80e6a4b47
George Theofilis <g.theofilis@clmsuk.com>
2019-02-25 18:06:06 +0200

    Fix alert tests

--- tests/model/test_alert.py
+++ tests/model/test_alert.py
@@ -5,31 +5,33 @@ from chariot_base.model import Alert
 
 
 def test_default():
-    alert = Alert()
+    alert = Alert('topology')
     assert alert.message is None
     assert alert.timestamp is not None
     assert alert.severity == 100
 
 
 def test_message():
-    alert = Alert('Message')
+    alert = Alert('topology', 'Message')
     assert(alert.message == 'Message')
     assert(alert.timestamp is not None)
     assert(alert.severity == 100)
 
 
 def test_severity():
-    alert = Alert('Message', 70)
+    alert = Alert('topology', 'Message', 70)
     assert(alert.message == 'Message')
     assert(alert.timestamp is not None)
     assert(alert.severity == 70)
 
 
 def test_severity_string():
-    alert = Alert('Message', 70)
-    assert(str(alert) == '{"message": "Message", "severity": 70}')
+    alert = Alert('topology', 'Message', 70)
+    alert.timestamp = '2019-02-25T16:03:33.474156'
+    assert(str(alert) == '{"name": "topology", "message": "Message", "severity": "70", "timestamp": "2019-02-25T16:03:33.474156"}')
 
 
 def test_severity_unicode():
-    alert = Alert('Message', 70)
-    assert(alert.__unicode__() == '{"message": "Message", "severity": 70}')
+    alert = Alert('topology', 'Message', 70)
+    alert.timestamp = '2019-02-25T16:03:33.474156'
+    assert(alert.__unicode__() == '{"name": "topology", "message": "Message", "severity": "70", "timestamp": "2019-02-25T16:03:33.474156"}')
