28d26cfa2580f060f1fcd7f4d08fc4661b0e553a
George Theofilis <g.theofilis@clmsuk.com>
2019-03-28 12:42:36 +0200

    Add database section

--- chariot_base/utilities/configuration.py
+++ chariot_base/utilities/configuration.py
@@ -8,13 +8,13 @@ import logging.config
 
 default_logging = {
     "version": 1,
-    "handlers": {            
+    "handlers": {
         "console": {
             "class": "logging.StreamHandler",
             "formatter": "console"
         }
     },
-    "loggers": {            
+    "loggers": {
     },
     "root": {
         "level": "INFO",
@@ -35,26 +35,28 @@ class Brokers(object):
         self.southbound = options.get('southbound', {})
         self.northbound = options.get('northbound', {})
 
+
 class Configuration(object):
     def __init__(self, options):
-        self.cloudant = options.get('cloudant', { "enabled": False })
-        self.watson_iot =  options.get('watson_iot', { "enabled": False })
-        self.local_storage =  options.get('local_storage', { "enabled": False })
-        self.brokers =  Brokers(options.get('brokers', {}))
-        self.tracer =  options.get('tracer', { "enabled": False })
-        self.privacy_engine =  options.get('privacy_engine', None)
-        self.alert_digester =  options.get('alert_digester', None)
-        self.dispatcher =  options.get('dispatcher', None)
-        self.northbound_dispatcher =  options.get('northbound_dispatcher', None)
-        self.topology =  options.get('topology', None)
-        self.set_logging( options.get('logging', default_logging))
+        self.cloudant = options.get('cloudant', {"enabled": False})
+        self.watson_iot = options.get('watson_iot', {"enabled": False})
+        self.local_storage = options.get('local_storage', {"enabled": False})
+        self.brokers = Brokers(options.get('brokers', {}))
+        self.tracer = options.get('tracer', {"enabled": False})
+        self.privacy_engine = options.get('privacy_engine', None)
+        self.alert_digester = options.get('alert_digester', None)
+        self.dispatcher = options.get('dispatcher', None)
+        self.northbound_dispatcher = options.get('northbound_dispatcher', None)
+        self.topology = options.get('topology', None)
+        self.database = options.get('database', None)
+        self.set_logging(options.get('logging', default_logging))
 
     def set_logging(self, options):
         self.logging = options
         logging.config.dictConfig(self.logging)
 
 
-def open_config_file(files = ['./config.json', './tests/config.json']):
+def open_config_file(files=['./config.json', './tests/config.json']):
     filename = None
     for name in files:
         if os.path.isfile(name):
