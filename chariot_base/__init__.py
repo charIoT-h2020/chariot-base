d5502e72da51571242516f27580a1d03527747ac
George Theofilis <g.theofilis@clmsuk.com>
2020-01-30 14:10:58 +0000

    Bump version: 0.12.0 → 0.12.1

--- chariot_base/__init__.py
+++ chariot_base/__init__.py
@@ -3,7 +3,7 @@
 Chariot base modules
 """
 
-__version__ = '0.12.0'
+__version__ = '0.12.1'
 
 from .connector import *
 from .datasource import *
--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/chariot-h2020/chariot_base',
-    version='0.12.0',
+    version='0.12.1',
     zip_safe=False,
 )
