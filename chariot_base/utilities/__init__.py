d30a3c1b12543efb86000a421780078d090392b2
George Theofilis <theofilis.g@gmail.com>
2020-02-13 08:29:14 +0000

    Expose topology wrapper

--- chariot_base/utilities/__init__.py
+++ chariot_base/utilities/__init__.py
@@ -3,3 +3,4 @@ from .permission import has_read_right, has_write_right
 from .tracing import Tracer, Traceable
 from .parsing import try_parse
 from .health import HealthCheck
+from .topology import Topology
