6ca9170938b8134b34c4add49d00df7932450c7f
George Theofilis <theofilis.g@gmail.com>
2020-02-14 16:01:41 +0000

    Add Jaeget Flask middleware

---
+++ chariot_base/middleware/tracing.py
@@ -0,0 +1,21 @@
+from werkzeug.wrappers import Request, Response, ResponseStream
+from chariot_base.utilities.tracing import Tracer, Traceable
+
+class TracingMiddleware(Traceable):
+    '''
+    Jaeger Tracing middleware
+    '''
+
+    def __init__(self, app, options):
+        self.app = app
+        self.tracer = None
+        self.set_up_tracer(options)
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        
+        span = self.start_span_from_request('request', request)
+        self.set_tag(span, 'path', request.path)
+        result = self.app(environ, start_response)
+        self.close_span(span)
+        return result
