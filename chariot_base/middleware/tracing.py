19cb694bc5a8cf95f106631bc6079b3fca26d074
George Theofilis <theofilis.g@gmail.com>
2020-02-19 13:05:04 +0000

    Inject tracer to request enviroment

--- chariot_base/middleware/tracing.py
+++ chariot_base/middleware/tracing.py
@@ -16,6 +16,8 @@ class TracingMiddleware(Traceable):
         
         span = self.start_span_from_request('request', request)
         self.set_tag(span, 'path', request.path)
+        environ['span'] = span
+        environ['trace'] = self
         result = self.app(environ, start_response)
         self.close_span(span)
         return result
